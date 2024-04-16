51bc6fce1086c8bfec802c88e8fe4830c4584549
Adnan Grifat <147456730+AdnanGrifat@users.noreply.github.com>
2024-04-15 19:10:29 -0500

    Update terraform.tf
    fix type

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,8 @@
-#
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  resource "aws_security_group" "allow_tls" {
+    name        = "allow_tls"
+    description = "Allow TLS inbound traffic"
+    vpc_id      = aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
